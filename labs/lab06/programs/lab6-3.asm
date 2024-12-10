01eacfd35b9608c59bff27fe9b441bf55cbf3118
Timurs  Bazhenovs <tbazhenovs@dk3n55.dk.sci.pfu.edu.ru>
2024-12-10 14:16:16 +0300

    d

--- labs/lab06/programs/lab6-3.asm
+++ labs/lab06/programs/lab6-3.asm
@@ -1,26 +1,28 @@
 %include 'in_out.asm' ; подключение внешнего файла
 SECTION .data
-div: DB 'Результат: ',0
-rem: DB 'Остаток от деления: ',0
+msg: DB 'Введите номер студенческого билета: ',0
+rem: DB 'Ваш вариант задания: ',0
+SECTION .bss
+x: RESB 80
 SECTION .text
 GLOBAL _start
 _start:
 ; ---- Вычисление выражения
-mov eax,5 ; EAX=5
-mov ebx,2 ; EBX=2
-mul ebx ; EAX=EAX*EBX
-add eax,3 ; EAX=EAX+3
-xor edx,edx ; обнуляем EDX для корректной работы div
-mov ebx,3 ; EBX=3
-div ebx ; EAX=EAX/3, EDX=остаток от деления
-mov edi,eax ; запись результата вычисления в 'edi'
-; ---- Вывод результата на экран
-mov eax,div call sprint mov eax,edi call iprintLF ; вызов подпрограммы печати
-; сообщения 'Результат: '
-; вызов подпрограммы печати значения
-; из 'edi' в виде символов
-mov eax,rem call sprint mov eax,edx call iprintLF ; вызов подпрограммы печати
-; сообщения 'Остаток от деления: '
-; вызов подпрограммы печати значения
-; из 'edx' (остаток) в виде символов
-call quit ; вызов подпрограммы завершения
+mov eax,msg
+call sprintLF
+mov ecx,x
+mov edx,80
+call sread
+
+mov eax,x
+call atoi; преобразуем asci в целое число
+xor edx,edx; обнуляем остаток
+mov ebx,20
+div ebx; eax = eax / ebx
+inc edx ;к остатку edx прибавляем 1
+
+mov eax,rem
+call sprint
+mov eax,edx
+call iprintLF
+call quit
